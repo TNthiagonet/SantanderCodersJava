571c3776f83fd82e92ff246dcea0370252a1229d
TNthiagonet <thiago@zn11f.onmicrosoft.com>
2024-06-13 16:39:35 -0300

    Concluindo módulo Java e enviando as aulas 23 e 24

---
+++ src/SantanderCoders/_23Funcoes.java
@@ -0,0 +1,14 @@
+package SantanderCoders;
+
+public class _23Funcoes {
+    public static void main(String[] args){
+        String nomeEmpresa = "ThiagoNET";
+        // FUNÇÕES
+        saudacao(nomeEmpresa);
+    }
+
+    // Saudação
+    public static void saudacao(String nomeParametro) {
+        System.out.println("Hello, " + nomeParametro);
+    }
+}
